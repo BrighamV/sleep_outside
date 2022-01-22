e01664666098043e8fadfaff974aca3a0305a321
Brenton <brentontreb@gmail.com>
2022-01-22 14:25:18 -0700

    Added cart total functionality

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,11 +6,28 @@ function getLocalStorage(key) {
 
 function getCartContents() {
   let markup = "";
+  let total = 0;
   // this needs to be made into an array (did this in product.js)
   const cartItems = getLocalStorage("so-cart");
+  // calculate the total of the items
+  for (var items of cartItems) {
+    total += items.FinalPrice;
+  }
+  // render the total if there are items in the cart
+  if (total > 0) {
+    renderTotal(total);
+  }
   const htmlItems = cartItems.map((item) => renderCartItem(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+}
+
+// create a render total function!!!
+function renderTotal(total) {
+  // remove the hide class
+  document.querySelector(".cart-footer").classList.remove("hide");
+  // render the total in the cart
+  document.querySelector(".cart-total").innerHTML = `Total: $${total}`;
 }
 
 function renderCartItem(item) {
