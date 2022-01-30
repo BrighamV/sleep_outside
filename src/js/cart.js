b227a4e9d99d34db8afb6672171382b275ac7143
Brenton <brentontreb@gmail.com>
2022-01-29 21:39:20 -0700

    Started on remove from cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,13 @@
 function getLocalStorage(key) {
-  console.log(localStorage.getItem(key));
-  console.log(JSON.parse(localStorage.getItem(key)));
+  // console.log(localStorage.getItem(key));
+  // console.log(JSON.parse(localStorage.getItem(key)));
   return JSON.parse(localStorage.getItem(key));
 }
 
 function getCartContents() {
   let markup = "";
   let total = 0;
-  // this needs to be made into an array (did this in product.js)
+  
   const cartItems = getLocalStorage("so-cart");
   // calculate the total of the items
   for (var items of cartItems) {
@@ -19,15 +19,20 @@ function getCartContents() {
   }
   const htmlItems = cartItems.map((item) => renderCartItem(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.getElementById(".cart-card__remove").addEventListener("click", removeFromCart);
   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
-// create a render total function!!!
+// renders the total for the items in the cart
 function renderTotal(total) {
   // remove the hide class
   document.querySelector(".cart-footer").classList.remove("hide");
   // render the total in the cart
   document.querySelector(".cart-total").innerHTML = `Total: $${total}`;
+}
+
+function removeFromCart(id){
+  // console.log(id);
 }
 
 function renderCartItem(item) {
@@ -43,9 +48,11 @@ function renderCartItem(item) {
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__remove" id="${item.Id}">X</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-  console.log(newItem);
+  // document.getElementById(item.Id).addEventListener("click", removeFromCart);
+  // console.log(newItem);
   return newItem;
 }
 
