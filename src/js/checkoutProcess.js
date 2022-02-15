55be98d31ce044970551d9012c6598cf88d391b2
Brenton Trebilcock <tre15004@byui.edu>
2022-02-15 11:17:15 -0700

    Team activity 6, finished part 4

--- src/js/checkoutProcess.js
+++ src/js/checkoutProcess.js
@@ -18,11 +18,19 @@ export default class CheckoutProcess{
     }
     calculateItemSummary(){
         // calculate the number of items in the cart and the total of those items
-        
+        let numItems = 0;
+        for (var product of this.list){
+            this.itemTotal += product.FinalPrice;
+            numItems++;
+        }
+        // put the number of items and the itemTotal into the form
+
     }
+    // run this function when the zip code is filled in
     calculateOrdertotal() {
         // calculate the shipping and tax amounts. Then use them to along with the cart total to figure out the order total
-        
+        this.tax = (this.itemTotal * 0.06).toFixed(2);
+        this.shipping = 10 + ((this.list.length - 1) * 2);
         // display the totals.
         this.displayOrderTotals();
       }
